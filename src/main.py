727c7815a2dab00579083c633aa2d1f2601516c6
leisyarcia <leisyarcia@gmail.com>
2020-10-26 22:47:39 +0000

    First commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db,User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,58 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+
+@app.route('/todos/<username>', methods=['GET'])
+def get_todos(username):
+    tasks = Task.get_task_by_username(username)
+    return jsonify({
+        "message":f"These are the tasks available for user {username}",
+        "task":tasks
+    }), 200
+
+
+@app.route('/todos/<username>', methods=['POST'])
+def create_todos(username):
+    request_data= request.get_json()
+    task=Task(request_data["label"],request_data["done"],username)
+    task.save_to_data()
+    return jsonify({"Message":"Task created" })
+
+@app.route('/todos/<id>', methods=['PUT'])
+def update_todos(id):
+    task= Task.get_task_by_id(int(id))
+    request_data=request.get_json()
+    task.label=request_data["label"]
+    task.done=request_data["done"]
+    task.save_to_data()
+    return jsonify({
+        "Message":"Task updated",
+        "Task_updater":task.serialize()
+    })
+
+@app.route('/todos/<id>', methods=['DELETE'])
+def delete_todos(id):
+    task=Task.get_task_by_id(int(id))
+    if task is None: 
+        raise APIException("Id not found ", 404)
+    task.delete_from_database()
+    return jsonify({"Message":"Task deleted"})
+
+
+
+
+
+# @app.route('/todos/<username>', methods=['POST'])
+# def new_todo():
+#     return 
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
